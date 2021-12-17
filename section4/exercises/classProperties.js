80141b43ca30a404822978692f3792f23bc12ba1
Nikhila Balmoori <nbalmoori@gmail.com>
2021-12-17 11:26:52 -0700

    Add Section 4

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -13,13 +13,38 @@ Make sure to run the file with node in your command line.
   // Write a class skeleton
   // Create 2 object instances and store in variables
   // Log each variable to the console.
-  // You do not need any proerties at this time!!
+  // You do not need any properties at this time!!
 
 // Prompt 1: Dog
 
+class Dog{
+}
+
+var murphy = new Dog();
+console.log(murphy);
+
+var birdie = new Dog();
+console.log(birdie);
+
 // Prompt 2: Snack
 
+class Snack{
+}
+
+var apple = new Snack ();
+console.log(apple);
+var granolaBar = new Snack();
+console.log(granolaBar);
+
 // Prompt 3: Shirt
+
+class Shirt{
+}
+
+var item00001 = new Shirt ();
+console.log(item00001);
+var item00002 = new Shirt ();
+console.log(item00002);
 
 //-------------------
 // Part 2: Properties
@@ -34,9 +59,44 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog2{
+  constructor() {
+    this.breed = "Yorkie";
+    this.ageInYears = 9;
+    this.weightinPounds = 8;
+  }
+}
+
+var duckie = new Dog2();
+console.log(duckie);
+
 // Prompt 2: Snack
 
+class Snack2{
+  constructor() {
+    this.calories = 100;
+    this.carbsInGrams = 20;
+    this.proteinInGrams = 1;
+  }
+}
+
+var chips = new Snack2 ();
+console.log(chips);
+
+
 // Prompt 3: Shirt
+
+class Shirt2{
+  constructor() {
+    this.price = 45;
+    this.size = "XS";
+    this.type = "T-Shirt";
+  }
+}
+
+var item11111 = new Shirt2 ();
+console.log(item11111);
+
 
 //-------------------
 // Part 3: Dynamic Properties
@@ -52,6 +112,46 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog3{
+  constructor(breed, ageInYears, weightinPounds) {
+    this.breed = breed;
+    this.ageInYears = ageInYears;
+    this.weightinPounds = weightinPounds;
+  }
+}
+
+var penelope = new Dog3("Goldendoodle", 7, 50);
+console.log(penelope);
+
+var millie = new Dog3("Old English Sheepdog", 3, 45);
+console.log(millie);
+
 // Prompt 2: Snack
 
+class Snack3{
+  constructor(calories, carbsInGrams, proteinInGrams) {
+    this.calories = calories;
+    this.carbsInGrams = carbsInGrams;
+    this.proteinInGrams = proteinInGrams;
+  }
+}
+
+var banana = new Snack3 (80, 10, 2);
+console.log(banana);
+var cookie = new Snack3 (400, 40, 10);
+console.log(cookie);
+
 // Prompt 3: Shirt
+
+class Shirt3{
+  constructor(price, size, type) {
+    this.price = price;
+    this.size = size;
+    this.type = type;
+  }
+}
+
+var item12345 = new Shirt3 (55, "Medium", "Crew Neck");
+console.log(item12345);
+var item54321 = new Shirt3 (72, "Large", "V-Neck");
+console.log(item54321);
--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -9,5 +9,43 @@ tweet object instances _behave_ as expected.
 */
 
 class Tweet {
+constructor(author, content, timeStamp) {
+  this.author = author;
+  this.content = content;
+  this.timeStamp = timeStamp;
+  this.numberOfLikes = 0;
+  this.comments = [];
+}
+incrementLikes() {
+  this.numberOfLikes++
+}
 
+addComment(newComment) {
+  this.comments.push(newComment)
+}
 };
+
+var tweet1 = new Tweet ("@jovan", "You're telling me a Shrimp fried this rice?", "11/19/2020 21:02:11");
+var tweet2 = new Tweet ("@caucasianjames", "Y'all mind if I have a good week?", "12/13/2021 09:01:00");
+var tweet3 = new Tweet ("@coffee_dad", "Need a coffee", "12/15/2020, 07:00:01")
+
+
+tweet1.incrementLikes()
+tweet1.incrementLikes()
+tweet1.addComment("Shut up")
+tweet2.incrementLikes()
+tweet2.addComment("No")
+tweet2.addComment("Yes")
+tweet3.incrementLikes()
+tweet1.addComment("LOL")
+tweet3.incrementLikes()
+tweet2.addComment("Maybe")
+tweet2.incrementLikes()
+tweet1.incrementLikes()
+tweet3.incrementLikes()
+tweet3.addComment("Me too dad")
+tweet3.incrementLikes()
+
+console.log(tweet1);
+console.log(tweet2);
+console.log(tweet3);
